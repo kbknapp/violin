825ce74949043bbf681bacb2a74870a7e32030c6
Kevin K <kbknapp@gmail.com>
2021-08-20 09:15:32 -0400

    adds a basic (lacking) example

---
+++ examples/basic.rs
@@ -0,0 +1,26 @@
+use rand::distributions::Distribution;
+use violin::{Node, VecN};
+
+fn main() {
+    // Create a node with an 8-Dimensional coordinate
+    let mut node = Node::<VecN<8>>::new();
+    let mut peer = Node::<VecN<8>>::new();
+
+    // Generate "random" RTTs (these would normally be determined from real latencies)
+    let mut rng = rand::thread_rng();
+    let die = rand::distributions::Uniform::from(1..5000); // synthetic latenies will range from 1 to 5000 ms
+    let rtts: Vec<u64> = vec![0u64; 2 * 1000] // 1000 synthetic latencies for each node
+        .iter_mut()
+        .map(|_rtt| die.sample(&mut rng))
+        .collect();
+
+    for lats in rtts.chunks(2) {
+        node.update(lats[0], peer.coordinate(), 25);
+        peer.update(lats[1], node.coordinate(), 25);
+    }
+
+    println!("node coordinates: {:?}", node.coordinate());
+    println!("peer coordinates: {:?}", peer.coordinate());
+    println!("---");
+    println!("Estimated Distance: {}", node.distance(peer.coordinate()));
+}
