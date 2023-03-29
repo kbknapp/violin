34d470336bee1bb447202be683c9fa59f11591ab
Kevin K <kbknapp@gmail.com>
2023-03-28 22:11:07 -0400

    feat: impl error with std feature

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,8 @@
 //! This module defines the `Result<T>` type alias and internal `Error` type
 
 use crate::std::result::Result as StdResult;
+#[cfg(feature = "std")]
+use crate::std::{error::Error as StdError, fmt};
 
 /// A type alias for returning `Result<T, violin::error::Error>`
 pub type Result<T> = StdResult<T, Error>;
@@ -23,3 +25,15 @@ impl Error {
     /// Returns true if the error is due to an invalid coordinate
     pub fn is_invalid(&self) -> bool { self.kind == ErrorKind::InvalidCoordinate }
 }
+
+#[cfg(feature = "std")]
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.kind {
+            ErrorKind::InvalidCoordinate => write!(f, "invalid coordinate"),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl StdError for Error {}
